88e0f3402dcf1e47323d5f9522e3d13eba736149
dfbq91 <dfbq91@gmail.com>
2020-02-03 14:07:42 -0500

    conditions, again

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,13 +9,13 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = (*list)->next, *prev, *next;
 
+	if (list == NULL)
+		return;
+
 	if (*list == NULL)
 		return;
 
-	if (**list == NULL)
-		return;
-
-	if (list->next == NULL)
+	if ((*list)->next == NULL)
 		return;
 
 	while (current)
