a2dced4df53b93bb601f0565b110f4581fe76835
dfbq91 <dfbq91@gmail.com>
2020-02-03 09:54:56 -0500

    new features

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,47 @@
 #include "sort.h"
 
 /**
- * swap - swaps the values of two integers
- * @number_one: pointer to the first value
- * @number_two: pointer to the second value
- * return: nothing
- */
-void swap(int *number_one, int *number_two)
-{
-	int aux;
-
-	aux = *number_one;
-	*number_one = *number_two;
-	*number_two = aux;
-}
-
-/**
  * insertion_sort_list - sorts an array of ints with dlinked list
  * @list: head node of the doubly linked list
  * return: nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list;
+	listint_t *current = (*list)->next, *prev, *next;
 
-	while (current->next != NULL)
+
+	while (current)
 	{
-		if (current > current->next)
+		if (current->prev && current->n < current->prev->n)
 		{
-			current->next = current->prev;
-			current->prev->prev = current->prev;
-			
-				
-		}
-
-
-	}
-	if (size <= 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (array[j] > array[j + 1])
+			while (current->prev)
 			{
-				swap(&(array[j]), &(array[j + 1]));
-				print_array(array, size);
+				prev = current->prev;
+				if (current->prev && current->n < current->prev->n)
+				{
+					if (current->prev->prev)
+						current->prev->prev->next = current;
+					next = current->next;
+					current->prev->next = next;
+					if (next)
+						next->prev = current->prev;
+					current->next = prev;
+					if (prev)
+					{
+						current->prev = prev->prev;
+						prev->prev = current;
+					}
+					else
+						current->prev = prev;
+					if (!current->prev)
+						*list = current;
+					print_list(*list);
+				}
+				else
+					break;
 			}
+
 		}
+		current = current->next;
 	}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers with selection algorithm
+ * @array: integers to be ordered
+ * @size: size of array
+ * return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, index;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		temp = array[i];
+		index = i;
+		for (j = i + 1; j < size; j++)
+		{
+
+			if (temp > array[j])
+			{
+				temp = array[j];
+				index = j;
+			}
+		}
+		array[index] = array[i];
+		array[i] = temp;
+		if (i != index)
+			print_array(array, size);
+	}
+}
